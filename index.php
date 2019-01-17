344f025bc3a162f6ba02b43e1f8a4288b0b197d2
Albert Francis <albertef@gmail.com>
2019-01-17 21:04:38 +0530

    added trained and skilled section

--- index.php
+++ index.php
@@ -1,16 +1,32 @@
 <?php include ("includes/header.php"); ?>
 
-<div class="banner-form">
-    <div class="banner-container col-xs-12 col-sm-12 col-md-7 col-md-offset-2 col-lg-7 col-lg-offset-2 no-padding text-center">
-        <?php include ("includes/banner.php"); ?>
+<div class="col-xs-12 col-sm-12 col-md-10 col-md-offset-2 col-lg-10 col-lg-offset-2 no-padding">
+    <div class="banner-form">
+        <div class="banner-container col-xs-12 col-sm-12 col-md-8 col-lg-8 no-padding text-center">
+            <?php include ("includes/banner.php"); ?>
+        </div>
+        <div class="home-form-container col-xs-12 col-sm-12 col-md-4 col-lg-4 no-padding text-left">
+            <ul class="second-menu">
+                <li class="hide"><a>Student Login</a> |</li>    
+                <li><a href="#">AMD Blog</a> |</li>
+                <li><a href="#">Careers</a></li>
+            </ul>
+            <?php include ("includes/form.php"); ?>
+        </div>
     </div>
-    <div class="home-form-container col-xs-12 col-sm-12 col-md-3 col-lg-3 no-padding text-left">
-        <ul class="second-menu">
-            <li class="hide"><a>Student Login</a> |</li>    
-            <li><a href="#">AMD Blog</a> |</li>
-            <li><a href="#">Careers</a></li>
-        </ul>
-        <?php include ("includes/form.php"); ?>
+
+    <div class="trained-skill">
+        <div class="col-xs-12 col-sm-12 col-md-5 col-lg-5">
+            <h3>
+                Professionally trained.<br>
+                Professionally skilled.
+            </h3>
+            <p>Adobe certification is the absolute best way to <br>communicate your proficiency in Adobe products.</p>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-5 col-lg-5">
+            <img src="img/ACTA.jpg" class="img-responsive">
+            <img src="img/approved-partners.png" class="img-responsive">
+        </div>
     </div>
 </div>
 
