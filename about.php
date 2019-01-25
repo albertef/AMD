40f7a94288f3c3433a292ebf83e13ea0208b8529
Albert Francis <albertef@gmail.com>
2019-01-25 23:38:04 +0530

    about page updates

--- about.php
+++ about.php
@@ -21,6 +21,72 @@
         </div>
     </div>
 
+    <div class="the-edge">
+        <div class="col-xs-12 col-sm-12 col-md-7 col-lg-7 no-padding edge-left">
+            <div class="about-inner-left">
+                <h4>THE EDGE</h4>
+                <P>The workplace demands for digital media skills, using Adobe’s video, graphic, web or design software is on the rise. At AMD you’ll have the opportunity to build and polish an outstanding set of skills ready to compete for creative jobs with the help of Adobe certifications that help educators effectively teach and validate digital communications skills while providing students and creative professionals with valuable career credentials.</P>
+
+                <p>
+                    <span class="awards">
+                        <img src="img/ACTA.jpg" class="img-responsive">
+                        <img src="img/approved-partners.png" class="img-responsive">
+                    </span>
+                </p>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-5 col-lg-5 no-padding edge-right">
+            <div class="about-inner-right welcome-inner-right">
+                <p>
+                    <ul>
+                        <li>Adobe certification is the absolute best way to communicate your proficiency in Adobe products.</li>
+                        <li>Adobe certified professionals can display Adobe certification logos on their business cards, resumes, and websites to leverage the power of the Adobe brand.</li>
+                        <li>Resumes with Adobe certifications get noticed and fast-tracked by hiring managers.</li>
+                        <li>Any other certifications if any and the importance they hold.</li>
+                    </ul>
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="ceo-message">
+        <div class="col-xs-12 col-sm-12 col-md-7 col-lg-7 no-padding ceo-left">
+            <div class="about-inner-left">
+                <h4>CEO'S MESSAGE</h4>
+                <P>The creative industries are one of the fastest growing sectors of the national and worldwide economy. Every year the demand for talent increases. The ability to think, to collaborate and to innovate will be key to building a solid foundation for a strong career. The workplace of the future will look different from what it seems today. The ability to build a ‘creative bridge’ between traditional and currently trending design will be highly valued.</P>
+
+                <P>Here at AMD, we believe that your time at the institution is the start of your career. Your own career prospects will be greatly enhanced under expert tuition and by making use of industry standard resources.</P>
+
+                <P>Our ambition is to deliver world-class education, with industry-focused courses, amazing facilities and academic staff and to build an institution that recognizes creativity as the most valuable skill and talent for the future.</P>
+
+                <P>Our mission is simple – to help you get a great job in the creative industry. We have a new beginning ahead of us; no one stands still – everyone looks forward. This is what will make AMD stand out from the crowd.</P>
+
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-5 col-lg-5 no-padding ceo-right">
+            <div class="about-inner-right ceo-inner-right">
+                <p>
+                    <img src="img/power-house/img1.jpg" class="img-responsive">
+                    <div class="ceo-testimonials">
+                        <h5>
+                            Philip Thomas<br>
+                            Managing Director
+                        </h5>
+                        <span>
+                            At AMD, we believe that your time at the institution is the start of your career.
+                        </span>
+                        <div class="social-icons">
+                            <a href="http://www.facebook.com" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                            <a href="http://www.instagram.com" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                            <a href="http://www.twitter.com" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                            <a href="http://www.linkedin.com" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                        </div>
+                    </div>
+                </p>
+            </div>
+        </div>
+    </div>
+
 
 
 
